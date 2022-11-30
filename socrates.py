b092821770b13ae7494965d2802f2d59e32a2b2a
Bicrick <patrickbrown@utexas.edu>
2022-11-29 21:35:42 -0600

    Chat with prompt through prompt.txt

--- socrates.py
+++ socrates.py
@@ -6,9 +6,9 @@ def read_key(file):
     with open(file, 'r', encoding='utf-8') as f:
         return f.read()
 
-openai.api_key = read_key('.apikey')
-print(read_key('.apikey'))
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
+openai.api_key = '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'
+#print(read_key('.apikey'))
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
@@ -20,9 +20,11 @@ def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, toke
         presence_penalty=pres_pen,
         stop=stop)
     text = response['choices'][0]['text'].strip()
-    return 
+    return text
 
 if __name__ == '__main__':
-    prompt = 'Who was the greatest boxer:'
+    file = open('prompt.txt', 'rt')
+    contents = file.read()
+    prompt = contents
     response = gpt3_completion(prompt)
     print(response)
