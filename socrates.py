6d1875609baaafd3dd3a052ff1fb5c9330899e02
anderdc <alexanderdcastaneda@gmail.com>
2022-11-29 21:02:11 -0600

    added stuff

--- socrates.py
+++ socrates.py
@@ -1,1 +1,28 @@
 import transformers
+import openai
+
+
+def read_key(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        return f.read()
+
+openai.api_key = read_key('.apikey')
+print(read_key('.apikey'))
+def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
+    prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
+    response = openai.Completion.create(
+        engine=engine,
+        prompt=prompt,
+        temperature=temp,
+        max_tokens=tokens,
+        top_p=top_p,
+        frequency_penalty=freq_pen,
+        presence_penalty=pres_pen,
+        stop=stop)
+    text = response['choices'][0]['text'].strip()
+    return 
+
+if __name__ == '__main__':
+    prompt = 'Who was the greatest boxer:'
+    response = gpt3_completion(prompt)
+    print(response)
