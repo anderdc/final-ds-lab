b7a67c13afb3b6c236c9276cbf2fbc3204be26a1
anderdc <alexanderdcastaneda@gmail.com>
2022-12-01 16:18:32 -0600

    push with apology

--- socrates_chat.py
+++ socrates_chat.py
@@ -1,5 +1,7 @@
 import openai
 
+person = 'JUDGE'
+
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
@@ -7,7 +9,7 @@ def open_file(filepath):
 openai.api_key = open_file('.apikey')
 
 
-def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['SOCRATES:', 'STUDENT:']):
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['SOCRATES:', f'{person}:']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
@@ -24,8 +26,8 @@ def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, toke
 if __name__ == '__main__':
     conversation = list()
     while True:
-        user_input = input('STUDENT: ')
-        conversation.append('STUDENT: %s' % user_input)
+        user_input = input(f'{person}: ')
+        conversation.append(f'{person}: %s' % user_input)
         text_block = '\n'.join(conversation)
         prompt = open_file('socrates_prompt.txt').replace('<<BLOCK>>', text_block)
         prompt = prompt + '\nSOCRATES:'
