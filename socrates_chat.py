91cd7c698379f1b520f91b86839b86a0518c1eba
Bicrick <patrickbrown@utexas.edu>
2022-12-01 16:52:39 -0600

    Transcript for Andrew Tate

--- socrates_chat.py
+++ socrates_chat.py
@@ -1,7 +1,6 @@
 import openai
-
+speaker = 'SOCRATES'
 person = 'JUDGE'
-
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
@@ -9,7 +8,7 @@ def open_file(filepath):
 openai.api_key = open_file('.apikey')
 
 
-def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['SOCRATES:', f'{person}:']):
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=[f'{speaker}:', f'{person}:']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
@@ -30,7 +29,7 @@ if __name__ == '__main__':
         conversation.append(f'{person}: %s' % user_input)
         text_block = '\n'.join(conversation)
         prompt = open_file('socrates_prompt.txt').replace('<<BLOCK>>', text_block)
-        prompt = prompt + '\nSOCRATES:'
+        prompt = prompt + f'\n{speaker}:'
         response = gpt3_completion(prompt)
-        print('SOCRATES:', response)
-        conversation.append('SOCRATES: %s' % response)
+        print(f'{speaker}:', response)
+        conversation.append(f'{speaker}: %s' % response)
