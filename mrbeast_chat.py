fab95671086035fd077e8471d50be59f334fe0b2
anderdc <alexanderdcastaneda@gmail.com>
2022-12-05 11:56:43 -0600

    mr beast final chat

--- mrbeast_chat.py
+++ mrbeast_chat.py
@@ -1,15 +1,15 @@
 import openai
-speaker = 'SOCRATES'      #GPT-3's persona
+speaker = 'MR. BEAST'      #GPT-3's persona
 person = 'STUDENT'
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
 openai.api_key = open_file('.apikey')
-tunedModel = 'davinci:ft-personal-2022-12-03-21-58-04'
+mrbeastModel = 'davinci:ft-personal-2022-12-04-22-58-41'
 eng = 'text-davinci-003'
 
-def gpt3_completion(prompt, engine=eng, temp=0.9, top_p=1.0, tokens=400, freq_pen=0.9, pres_pen=0.6, stop=[f'{speaker}:', f'{person}:']):
+def gpt3_completion(prompt, engine=mrbeastModel, temp=1, top_p=1.0, tokens=650, freq_pen=0.6, pres_pen=0.25, stop=[f'{speaker}:', f'{person}:']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
@@ -29,7 +29,7 @@ if __name__ == '__main__':
         user_input = input(f'{person}: ')
         conversation.append(f'{person}: %s' % user_input)
         text_block = '\n'.join(conversation)
-        prompt = open_file('socrates_prompt.txt').replace('<<BLOCK>>', text_block)
+        prompt = open_file('mrbeast_prompt.txt').replace('<<BLOCK>>', text_block)
         prompt = prompt + f'\n{speaker}:'
         response = gpt3_completion(prompt)
         print(f'{speaker}:', response)
